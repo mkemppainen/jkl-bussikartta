98a1c8b713358817d199f909503452333834ff72
Kasimir Ilmonen <jyrysanteri@hotmail.com>
2015-10-26 19:40:15 +0200

    Sain muistin käsittelyn luultavasti toimimaan.

--- rust/parsija_testi.py
+++ rust/parsija_testi.py
@@ -1,6 +1,12 @@
 import ctypes
 class Pysakki(ctypes.Structure):
     _fields_ = [("id", ctypes.c_void_p), ("nimi", ctypes.c_void_p), ("lat", ctypes.c_void_p), ("lon", ctypes.c_void_p)]
+    def poista(self):
+        lib.cstring_free(self.id)
+        lib.cstring_free(self.nimi)
+        lib.cstring_free(self.lat)
+        lib.cstring_free(self.lon)
+        return
 
 lib = ctypes.cdll.LoadLibrary("parsija/target/release/libparsija.so")
 lib.pysakki_free.argtypes = [ctypes.POINTER(Pysakki)]
@@ -15,9 +21,10 @@ print(esim1.id)
 print(esim1.nimi)
 print(esim1.lat)
 print(esim1.lon)
-lib.pysakki_free(esim1)
+esim1.poista()
 
-esim2 = lib.anna_esimerkki_teksti()
-print ctypes.cast(esim2, ctypes.c_char_p).value
-lib.cstring_free(esim2)
+#esim2 = lib.anna_esimerkki_teksti()
+#print ctypes.cast(esim2, ctypes.c_char_p).value
+#print(esim2)
+#lib.cstring_free(ctypes.cast(esim2, ctypes.c_void_p))
 print("Kala")
