e5464484c9e2312c8dac0859df76d98f80202db2
Jamuska <jamu246@gmail.com>
2015-10-28 15:36:17 +0200

    pikku muutos

--- server/flask_server.py
+++ server/flask_server.py
@@ -14,19 +14,15 @@ def hello():
 
 @app.route('/get_route', methods=['GET', 'POST'])
 def get_route():
-    print('success', file=sys.stderr)
+    print('success', file=sys.stderr) #testi
    
     if request.method == 'POST':
-        """
-        f = request.files['route_data']
-        
-        f.save('https://api.mapbox.com/v4/directions/mapbox.driving/25.70022,62.25287;25.70514,62.24816;25.736,62.244.json?access_token=pk.eyJ1IjoibWlra29rZW0iLCJhIjoiY2lmcDIwMDNlMDFpMnRha251dHgwbG9hZiJ9.9DLJHVEwbRf7xT0WkFqj5Q&steps=false')
-        """
-        
+                
         r = requests.get('https://api.mapbox.com/v4/directions/mapbox.driving/25.70022,62.25287;25.70514,62.24816;25.736,62.244.json?access_token=pk.eyJ1IjoibWlra29rZW0iLCJhIjoiY2lmcDIwMDNlMDFpMnRha251dHgwbG9hZiJ9.9DLJHVEwbRf7xT0WkFqj5Q&steps=false')
-        print('success2', file=sys.stderr)
+        print('success2', file=sys.stderr) #testi
         print(r.content, file=sys.stderr)
-        
+       
+
     return render_template('index.html')  
 if __name__ == '__main__':
    app.run(debug=True)
