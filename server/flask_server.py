88aef2874f33834b2d9e4c5e9907fac8102c5da8
Jamuska <jamu246@gmail.com>
2015-11-02 21:07:25 +0200

    Palvelin osa lahettaa selaimelle JSONIA jonkinlaisessa muodossa

--- server/flask_server.py
+++ server/flask_server.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
-from flask import Flask
+from flask import Flask, Response
 from flask import render_template
 from flask import request
+from flask import url_for, redirect, jsonify, make_response
 import requests
 import json
 import sys
@@ -9,20 +10,40 @@ import sys
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-   return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/get_route', methods=['GET', 'POST'])
+#reitti?time=12:30 saisiko tammosella avattua routen?
+#Pitaa muokata hakemaan dataa dynaamisesti, nyt palautuu staattinen
+#JSON-data
+@app.route("/get_route")
 def get_route():
-    print('success', file=sys.stderr) #testi
-   
-    if request.method == 'POST':
-                
+    if request.method == 'GET':
+        print('success', file=sys.stderr) # testi
         r = requests.get('https://api.mapbox.com/v4/directions/mapbox.driving/25.70022,62.25287;25.70514,62.24816;25.736,62.244.json?access_token=pk.eyJ1IjoibWlra29rZW0iLCJhIjoiY2lmcDIwMDNlMDFpMnRha251dHgwbG9hZiJ9.9DLJHVEwbRf7xT0WkFqj5Q&steps=false')
-        print('success2', file=sys.stderr) #testi
-        print(r.content, file=sys.stderr)
-       
+        
+        #MIKSI TAMA EI TALLENNU OIKEASSA MUODOSSA?
+        r2 = [{
+            "reitinNimi":"kuokkala",
+            "pysakinValit":[{
+            "lahtoNimi":"pysakki3",
+            "lahtoAika":"12:15",
+            "lahtoPiste":[62.25287, 25.70022],
+            "paateNimi":"pysakki17",
+            "paatePiste":[62.24816, 25.270514],
+            "paateAika": "12:20",
+            "duration": 300,
+            "coordinates":[[25.700226,62.252873],[25.700302,62.25277], 
+			  [25.700522,62.252304],[25.700153,62.251181],
+                           [25.700726,62.252204],[25.70153,62.252123]]}
+                        ]}]
 
-    return render_template('index.html')  
+    resp = Response(response=json.dumps(r2),
+        status=200,
+        mimetype="application/json")
+    print(resp.response, file=sys.stderr) #TESTI
+    return(resp)
+    
+
 if __name__ == '__main__':
    app.run(debug=True)
